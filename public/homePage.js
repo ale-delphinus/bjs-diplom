3bc7d421260a52a2f5e3b4480d9e83e8ffc757cd
Evteeva Ksenia <slepzova.ksenia@mail.ru>
2024-07-04 02:12:54 +0300

    bjs-diplom part done

---
+++ public/homePage.js
@@ -0,0 +1,106 @@
+'use strict';
+
+const logoutButton = new LogoutButton();
+
+logoutButton.action = () => {
+    ApiConnector.logout(response => {
+        if(response.sucsess) {
+            location.reload();
+        }
+    });
+}
+
+
+ApiConnector.current(response => {
+    if(response.sucsess) {
+        ProfileWidget.showProfile(response.data);
+    }
+});
+
+const ratesBoard = new RatesBoard();
+
+function getRatesBoard() {
+    ApiConnector.getStocks(response => {
+        if(response.sucsess) {
+            ratesBoard.clearTable();
+            ratesBoard.fillTable(response.data);
+        }
+    });
+}
+
+
+getRatesBoard();
+
+
+setInterval(getRatesBoard, 60000);
+
+
+const moneyManager = new MoneyManager();
+
+moneyManager.addMoneyCallback = (data) => {
+    ApiConnector.addMoney(data, response => {
+        if(response.success) {
+            ProfileWidget.showProfile(response.data);
+            moneyManager.setMessage(response.success, 'Баланс пополнен');
+        } else {
+            moneyManager.setMessage(response.error, 'Не удалось пополнить баланс');
+        }
+    });     
+}
+
+moneyManager.conversionMoneyCallback = (data) => {
+    ApiConnector.convertMoney(data, response => {
+        if(response.success) {
+            ProfileWidget.showProfile(response.data);
+            moneyManager.setMessage(response.success, 'Конвертация прошла успешно');
+        } else {
+            moneyManager.setMessage(response.error, 'Не удалось конвертировать валюту');
+        }
+    });     
+}
+
+moneyManager.sendMoneyCallback = (data) => {
+    ApiConnector.transferMoney(data, response => {
+        if(response.success) {
+            ProfileWidget.showProfile(response.data);
+            moneyManager.setMessage(response.success, 'Перевод выполнен');
+        } else {
+            moneyManager.setMessage(response.error, 'Не удалось произвести перевод');
+        }
+    });     
+}
+
+
+const favoritesWidget = new FavoritesWidget();
+
+let getFavorites = () => {
+    ApiConnector.getFavorites(response => {
+        if(response.success) {
+            favoritesWidget.clearTable();
+            favoritesWidget.fillTable(response.data);
+            moneyManager.updateUsersList(response.data);
+        }
+    });
+}
+
+favoritesWidget.addUserCallback = (data) => {
+    ApiConnector.addUserToFavorites(data, response => {
+        if(response.success) {
+            getFavorites();
+            favoritesWidget.setMessage(response.success, 'Пользователь добавлен в избранное');
+        } else {
+            favoritesWidget.setMessage(response.error, 'Не удалось добавить пользователя в избранное');
+        }
+    });
+}
+
+favoritesWidget.removeUserCallback = (data) => {
+    ApiConnector.removeUserFromFavorites(data, response => {
+        if(response.success) {
+            getFavorites();
+            favoritesWidget.setMessage(response.success, 'Пользователь удален из избранного');
+        } else {
+            favoritesWidget.setMessage(response.error, 'Не удалось удалить пользователя из избранного');
+        }
+    });
+}
